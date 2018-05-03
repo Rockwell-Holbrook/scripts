8ff14a847a6cbfa31d5160fa7f8ec342d4b024d7
Rockwell Holbrook <Rockwell.holbrook@gmail.com>
2018-05-03 13:58:26 -0600

    added additional functionality

--- transferJarScript.sh
+++ transferJarScript.sh
@@ -1,13 +1,30 @@
 #!/bin/bash
 
+#Variables: 
+sum=`md5sum aaaaa.txt`
+shortenedSum=`md5sum aaaaa.txt | cut -c1-6`
+date=`date +%Y-%m-%d`
+jarPath="accessmanager/target/accessma*-with-dependencies.jar"
+
 #Print the md5sum for record keeping
 echo "MD5SUM OF THE NEW JAR: " 
-echo md5sum IAAMAuto-1.0-SNAPSHOT-jar-with-dependencies.jar
+echo $sum 
+echo `ls -l accessmanager/target/accessm*.jar`
 
 #Do the Transfers
+echo " "
 echo "Transfer Jar to Slave-756"
-scp /mnt/c/Users/rholbrook/Documents/nam-git/iaam-auto/target/IAAMAuto-1.0-SNAPSHOT-jar-with-dependencies.jar root@prvqenam756.namdom022.lab:/root/NAMAutomationJars
+scp -p $jarPath root@prvqenam756.namdom022.lab:/root/NAMAutomationJars
+
+echo " "
 echo "Transfer Jar to Slave-758"
-scp /mnt/c/Users/rholbrook/Documents/nam-git/iaam-auto/target/IAAMAuto-1.0-SNAPSHOT-jar-with-dependencies.jar root@prvqenam758.namdom022.lab:/root/NAMAutomationJars
+scp -p $jarPath root@prvqenam758.namdom022.lab:/root/NAMAutomationJars
+
+echo " "
 echo "Transfer Jar to Slave-765"
-scp /mnt/c/Users/rholbrook/Documents/nam-git/iaam-auto/target/IAAMAuto-1.0-SNAPSHOT-jar-with-dependencies.jar root@prvqenam765.namdom022.lab:/root/NAMAutomationJars
+scp -p $jarPath root@prvqenam765.namdom022.lab:/root/NAMAutomationJars
+
+#Rename and keep a record. 
+echo " "
+read -p "Enter Jar Description Message (No Spaces Allowed): " description
+scp -p $jarPath root@prvqenam756.namdom022.lab:/root/NAMAutomationJars/NAMJar.$date.$shortenedSum.$description
